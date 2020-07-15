63f14d4b3523b13a8268fd2877e856fac9d947bc
unknown <bruna.yoshie@iventis.com.br>
2020-07-15 17:02:20 -0300

    first commit

---
+++ algoritmo1/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Text;
+
+namespace algoritmo1
+{
+    class Program
+    {
+        private static int l;
+
+        static void Main(string[] args)
+        { 
+        }
+    }
+}
+
+
+//    //        // Exercicio 3 - Jogo da Forca 
+
+//    //        int maxTentativas = 10;
+//    //        string palavra = "DESENVOLVEDOR";
+//    //        int qtdeCaracteres = palavra.Length;
+//    //        string segredo = "";
+//    //        int letrasAcertadas = 0;
+
+//    //        for (int i = 0; i < qtdeCaracteres; i++)
+//    //        {
+//    //            segredo = segredo + "_";
+//    //        }
+
+//    //        string letra = "";
+
+//    //        for (int i = 0; i < maxTentativas; i++)
+
+//    //            if (letrasAcertadas == palavra.Length)
+               
+//    //            {
+//    //                Console.WriteLine("Parabéns! Você Ganhou!");
+//    //                break;
+//    //            }
+
+//    //        Console.WriteLine(segredo);
+//    //        Console.WriteLine("Informe uma Letra:");
+//    //        letra = Console.ReadLine();
+//    //        letra = letra.ToUpper();
+
+//    //        int qtdeEspacos = 0;
+
+//    //        if (letra.Length == l)
+
+
+//    //            for (int l = 0; l < qtdeCaracteres; l++)
+//    //            {
+//    //                string letter = palavra.Substring(l, 1);
+
+//    //                if (l != 0)
+//    //                    qtdeEspacos++;
+
+//    //                if (letter == letra)
+
+//    //                {
+//    //                    letrasAcertadas++;
+//    //                    int indexsubstring;
+//    //                    if (l == 0)
+//    //                        indexsubstring = l;
+//    //                    else
+//    //                        indexsubstring = l + qtdeEspacos;
+
+//    //                    StringBuilder sb = new StringBuilder(segredo);
+//    //                    sb[indexsubstring] = Convert.ToChar(letter);
+//    //                    segredo = sb.ToString();
+//    //                }
+                
+
+
+//    //        else
+
+//    //        {
+//    //            if (letra.ToLower() == palavra.ToLower())
+
+//    //            {
+//    //                letrasAcertadas = palavra.Length;
+//    //                Console.WriteLine("Você Perdeu! Não foi desta vez!");
+//    //            }
+
+//    //        }
+//    //            }
+//    //    }
+//    //}
+
+////}
+
+
+
+
+
+
+
+
+//                // Exercicio 2 - tempo de viagem 
+
+//                //Console.WriteLine("Digite a distancia (km):");
+//                //double distancia = Convert.ToDouble(Console.ReadLine());
+
+//                //Console.WriteLine("Digite a velocidade (km/h):");
+//                //double velocidade= Convert.ToDouble(Console.ReadLine());
+
+//                //double tempo = distancia / velocidade;
+
+//                //Console.WriteLine($"Você chegara ao seu destino em {tempo} hrs");
+
+
+
+
+
+//                // Exercicio 1 - Nota do Aluno
+
+//                //Console.WriteLine("Digite a Primeira Nota:");
+//                //double nota1 = Convert.ToDouble(Console.ReadLine());
+
+//                //Console.WriteLine("Digite a Segunda Nota:");
+//                //double nota2 = Convert.ToDouble(Console.ReadLine());
+
+//                //Console.WriteLine("Digite a Terceira Nota:");
+//                //double nota3 = Convert.ToDouble(Console.ReadLine());
+
+//                //Console.WriteLine("Digite a Quarta Nota:");
+//                //double nota4 = Convert.ToDouble(Console.ReadLine());
+
+//                //double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+//                //Console.WriteLine(media);
+
+//                //if (media <= 6.9)
+//                //{
+//                //    Console.WriteLine($"A media do aluno foi:{media}");
+//                //    Console.WriteLine("- Aluno Reprovado");
+//                //}
+
+//                //if (media >= 7)
+//                //{
+//                //    Console.WriteLine($"A media do aluno foi:{media}");
+//                //    Console.WriteLine("- Aluno Aprovado");
+//                //}
+
+
+
+            
+        
+    
